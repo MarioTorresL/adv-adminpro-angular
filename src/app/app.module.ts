f69762a6098498b3023285887f7eb071c8e9ee3f
Mario Torres <mario.torreslepe@gmail.com>
2022-07-12 21:56:15 -0400

    add auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
+
     NotfoundComponent
   ],
   imports: [
